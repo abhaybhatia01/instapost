d13b71287abf0d0cd51f686013933fe401f00eda
Abhay Bhatia <abhay1732002@gmail.com>
2023-07-29 18:51:32 +0530

    update

--- index.js
+++ index.js
@@ -17,8 +17,27 @@ app.get('/meta', (req, res) => {
     console.error(error);
     });
 });
-app.get('/meta/auth', (req, res) => {
-   res.send("done");
+
+app.get('/meta/auth:code', (req, res) => {
+    const {code} = req.query;
+    fetch('https://api.example.com/datahttps://api.instagram.com/oauth/access_token', {
+        method: 'POST',
+        body: JSON.stringify({
+            client_id: '160358547066273',
+            client_secret: '48a9a0f67bc6a07fda26f99838a262df',
+            grant_type: 'authorization_code',
+            redirect_uri: 'https://instapost-beta.vercel.app/meta/auth',
+            code: code,
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        res.send(data)
+    })
+    .catch(error => {
+        console.error(error);
+    });
 });
 
 
