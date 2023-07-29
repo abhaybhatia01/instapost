0ef5c47d98de427f7374ed40b9054abd5edbf10e
Abhay Bhatia <abhay1732002@gmail.com>
2023-07-29 18:33:39 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
-app.get('/meta/auth', (req, res) => {
+app.get('/meta', (req, res) => {
     fetch("https://api.instagram.com/oauth/authorize?client_id=160358547066273&redirect_uri=https://instapost-beta.vercel.app/meta/auth&scope=user_profile,user_media&response_type=code")
     .then(response => response.json())
     .then(data => {
@@ -16,6 +16,9 @@ app.get('/meta/auth', (req, res) => {
     .catch(error => {
     console.error(error);
     });
+});
+app.get('/meta/auth', (req, res) => {
+   res.send("done");
 });
 
 
