d3f0f7046067e96a01e794f0cc26a293d9b80dfd
Abhay Bhatia <abhay1732002@gmail.com>
2023-07-29 18:31:04 +0530

    update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const app = express();
 
 
@@ -6,8 +7,19 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
+app.get('/meta/auth', (req, res) => {
+    fetch("https://api.instagram.com/oauth/authorize?client_id=160358547066273&redirect_uri=https://instapost-beta.vercel.app/meta/auth&scope=user_profile,user_media&response_type=code")
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+    })
+    .catch(error => {
+    console.error(error);
+    });
+});
 
-  const port = 3000; // or any other port number you prefer
-   app.listen(port, () => {
-     console.log(`Server is running on port ${port}`);
-   });
+
+const port = 3000; // or any other port number you prefer
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
